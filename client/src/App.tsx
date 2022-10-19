c86a3beb7bcc4587cfd894fcec9d5d1e56145c90
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-10-19 19:59:12 +0300

    basic idea works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+const ws = new WebSocket("ws://localhost:8080/");
 
 function App() {
-  return <div className="App">Test</div>;
+  const [text, setText] = useState("");
+
+  const handleSend = (evt: React.MouseEvent<HTMLInputElement>) => ws.send(text);
+
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) =>
+    setText(evt.target.value);
+
+  return (
+    <div>
+      <input type="text" id="message" value={text} onChange={handleChange} />
+      <input type="button" id="send" value="send" onClick={handleSend} />
+    </div>
+  );
 }
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,3 +4,11 @@ const wss = new WebSocketServer({
   port: 8080,
   path: "/",
 });
+
+wss.on("connection", (ws) => {
+  console.log("wss connection");
+  ws.on("open", () => console.log("ws open"));
+  ws.on("message", (data) => {
+    console.log(data.toString("utf8"));
+  });
+});
