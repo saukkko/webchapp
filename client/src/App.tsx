c394224e24b96cca7fdc36e8cbdc93ff51028b86
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-10-20 21:45:18 +0300

    added test cookies (not signed!) and renamed 'text' state variables to 'message'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
+
+// viedäänkö huone ja nick tieto esim. kekseissä?
+document.cookie = "room=testihuone";
+document.cookie = "nick=testinick";
+
 const ws = new WebSocket("ws://localhost:8080/");
 
 function App() {
-  const [text, setText] = useState("");
+  const [message, setMessage] = useState("");
 
-  const handleSend = (evt: React.MouseEvent<HTMLInputElement>) => ws.send(text);
+  const handleSend = (evt: React.MouseEvent<HTMLInputElement>) =>
+    ws.send(message);
 
   const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) =>
-    setText(evt.target.value);
+    setMessage(evt.target.value);
 
   return (
     <div>
-      <input type="text" id="message" value={text} onChange={handleChange} />
+      <input type="text" id="message" value={message} onChange={handleChange} />
       <input type="button" id="send" value="send" onClick={handleSend} />
     </div>
   );
