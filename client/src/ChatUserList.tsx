993e053412ac7919b02cffc3bf28111d975f16c9
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-11-22 21:59:24 +0200

    render user list only if there is any users

--- client/src/ChatUserList.tsx
+++ client/src/ChatUserList.tsx
@@ -1,15 +1,16 @@
-import { ChatUserListProps } from "./types";
+import type { ChatUserListProps } from "./types";
 
 export const ChatUserList: React.FC<ChatUserListProps> = ({ users }) => {
-	return (
-		<div id="user-list" className="w-1/5 px-4 overflow-y-auto">
-			<div className="text-xl">Users</div>
-			<ul className="list-none">
-				{users.map((u, i) => (
-					<li key={i}>{u}</li>
-				))}
-			</ul>
-		</div>
-	);
-}
-
+  return users ? (
+    <div id="user-list" className="w-1/5 px-4 overflow-y-auto">
+      <div className="text-xl">Users</div>
+      <ul className="list-none">
+        {users.map((u, i) => (
+          <li key={i}>{u.nick}</li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <></>
+  );
+};
