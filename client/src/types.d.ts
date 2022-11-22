6689fd1e6a791cb205942c4fc378b26b9eb25817
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-11-22 21:44:10 +0200

    more typings

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -9,15 +9,19 @@ declare type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 declare type ChatWindowProps = HTMLAttributes<HTMLDivElement>;
 
+declare type ChatUsers = {
+  nick: string;
+};
+
 declare type ChatUserListProps = HTMLAttributes<HTMLUListElement> & {
-  users: string[];
+  users: ChatUsers[];
 };
 
 declare type ChatMessageListProps = HTMLAttributes<HTMLUListElement> & {
-  chatLog: string[];
+  chat: ChatSettings;
 };
 declare type ChatMessageProps = LiHTMLAttributes<HTMLLIElement> & {
-  msg: { username: string; text: string };
+  data: { username: string; text: string };
 };
 declare type ChatMessageUserProps = LiHTMLAttributes<HTMLLIElement> & {
   username: string;
@@ -26,13 +30,64 @@ declare type ChatMessageUserProps = LiHTMLAttributes<HTMLLIElement> & {
 declare type ChatProps = {
   chatWindowProps: ChatWindowProps;
   chatMessageListProps: ChatMessageListProps;
+  chatUserListProps: ChatUserListProps;
 };
 
-export {
+declare type ChatAppProps = User & {
+  ws: WebSocket;
+};
+
+declare type ChatSettings = {
+  chatLog: WSMessageData[];
+  nick: string;
+  room: string;
+  users: ChatUsers[];
+};
+
+declare type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+};
+
+declare type WSMessageObject = {
+  type: WSMessageType;
+  token?: string;
+  timestamp: number;
+  users: ChatUsers[];
+  data: WSMessageData;
+};
+
+declare type WSHeartbeatObject = {
+  type: "heartbeat" | "ack";
+  users?: ChatUsers[];
+};
+
+type WSMessageType = "message" | "token" | "update";
+type WSData = WSMessageData;
+
+declare type WSMessageData = {
+  nick: string;
+  msg: string;
+};
+
+declare type User = {
+  room: string;
+  nick: string;
+};
+
+export type {
+  User,
+  ButtonProps,
   ChatWindowProps,
+  ChatUsers,
   ChatUserListProps,
   ChatMessageListProps,
   ChatMessageProps,
   ChatMessageUserProps,
   ChatProps,
+  ChatAppProps,
+  ChatSettings,
+  WSMessageObject,
+  WSHeartbeatObject,
+  WSMessageData,
+  InputProps,
 };
